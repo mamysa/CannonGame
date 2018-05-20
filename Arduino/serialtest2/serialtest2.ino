10c20ac978f6259a427574bc4ae912bd3d9f8125
mamysa <alexmamyshev@fastmail.com>
2018-05-20 15:32:25 +0200

    serialevent handler

--- Arduino/serialtest2/serialtest2.ino
+++ Arduino/serialtest2/serialtest2.ino
@@ -1,7 +1,7 @@
 #define RECVBUFSIZE 512
 char recvBuf[RECVBUFSIZE] = { '\0' };
 int  recvBytes = 0;
-
+bool string_complete = false;
 
 int angle = 12;
 int force = 1687;
@@ -14,12 +14,18 @@ void setup() {
 }
 
 void loop() {
-  recv();
+ // recv();
+ snd();
+ if (string_complete) {
+  parseMsg();
+ }
 }
 
 void snd() {
   Serial.print("Prm ");
   Serial.print(String(angle));
+  Serial.print(" ");
+  Serial.print(String(force));
   Serial.print("\n");
 }
 
@@ -28,20 +34,9 @@ void snd_ack() {
   Serial.print("\n");
 }
 
-void recv() {
-  while (Serial.available() > 0) {
-    char chr = Serial.read();
-    if (chr == '\n') {
-      recvBuf[recvBytes++] = '\0';
-      parseMsg();
-      recvBytes = 0;
-      return;
-    }
-    recvBuf[recvBytes++] = chr;
-  }
-}
-
 void parseMsg() {
+  recvBytes = 0;
+  string_complete = false;
   if (recvBuf[0] == 'P' && recvBuf[1] == '1') {
     
     char *header = strtok(recvBuf, " ");
@@ -51,10 +46,12 @@ void parseMsg() {
     int p1 = atoi(param1);
     int p2 = atoi(param2);
 
-    Serial.print("Param1 is: ");
-    Serial.println(p1);
-    Serial.print("Param2 is: ");
-    Serial.println(p2);
+    
+    
+    
+    angle = p1;
+    force = p2;
+    
   }
   else if (recvBuf[0] == 'P' && recvBuf[1] == 'R' && recvBuf[2] == 'M') {
     snd();
@@ -65,7 +62,18 @@ void parseMsg() {
   else {
     Serial.println("Unknown message");
   } 
+  
 }
 
+void serialEvent() {
+  while (Serial.available() > 0) {
+    char chr = Serial.read();
+    if (chr == '\n') {
+      recvBuf[recvBytes++] = '\0';
+      string_complete = true;
+    }
+    recvBuf[recvBytes++] = chr;
+  }
+}
 
 
